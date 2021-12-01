02b78c73e56177c3b3154e9adff3ab58cd556a9d
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-11-30 21:57:54 -0500

    Implement User schema

---
+++ model/User.js
@@ -0,0 +1,130 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    type: Map,
+    of: new Schema({
+        schemaVersion: Number,
+        id: String,
+        password: String,
+        name: String,
+        email: String,
+        retroCalendar: {
+            id: String,
+            googleId: String
+        },
+        workspaces: {
+            type: Map,
+            of: new Schema ({
+                color: Number,
+                projects: {
+                    type: Map,
+                    of: new Schema({
+                        color: Number,
+                        tags: {
+                            byId: {
+                                type: Map,
+                                of: new Schema({
+                                    name: String,
+                                    color: Number
+                                })
+                            },
+                            index: [
+                                {
+                                    type: Map,
+                                    of: new Schema({
+                                        type: Map,
+                                        of: Boolean
+                                    })
+                                }
+                            ]
+                        },
+                        tagCompositions: {
+                            index: {
+                                type: Map,
+                                of: new Schema({
+                                    type: Map,
+                                    of: Boolean
+                                })
+                            },
+                            byId: {
+                                type: Map,
+                                of: new Schema({
+                                    tags: [String],
+                                    tasks: [String],
+                                    durationSum: mongoose.Decimal128,
+                                    numberOfTasks: Number,
+                                    durationAverage: mongoose.Decimal128,
+                                    durationStandardDeviation: mongoose.Decimal128,
+                                    durationEstimate: mongoose.Decimal128,
+                                    durationInitialEstimate: mongoose.Decimal128
+                                })
+                            }
+                        },
+                        tasks: {
+                            byId: {
+                                type: Map,
+                                of: new Schema({
+                                    name: String,
+                                    description: String,
+                                    billingType: String,
+                                    tags: {
+                                        type: Map,
+                                        of: Boolean
+                                    },
+                                    logs: {
+                                        type: Map,
+                                        of: new Schema({
+                                            type: Map,
+                                            of: new Schema({
+                                                duration: mongoose.Decimal128
+                                            })
+                                        })
+                                    },
+                                    actualDuration: mongoose.Decimal128,
+                                    creationDate: Date,
+                                    completionDate: Date,
+                                    firstLogDate: Date,
+                                    lastLogDate: Date,
+                                    assignedDuration: mongoose.Decimal128
+                                }),
+                            },
+                            index: {
+                                byState: {
+                                    todo: {
+                                        type: Map,
+                                        of: Boolean
+                                    },
+                                    inProgress: {
+                                        type: Map,
+                                        of: Boolean
+                                    },
+                                    done: {
+                                        type: Map,
+                                        of: Boolean
+                                    }
+                                },
+                                byDeadline: {
+                                    type: Map,
+                                    of: new Schema({
+                                        type: Map,
+                                        of: Boolean
+                                    })
+                                },
+                                byTag: {
+                                    type: Map,
+                                    of: new Schema({
+                                        type: Map,
+                                        of: Boolean
+                                    })
+                                }
+                            }
+                        }
+                    })
+                }
+            })
+        }
+    })
+});
+
+module.exports = mongoose.model('User', UserSchema);
