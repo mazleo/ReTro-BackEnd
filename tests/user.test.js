e90e94e5a652962e76b0d4b97bdcf3498d9e8e78
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-12-06 21:41:04 -0500

    Create POST /user endpoint test

--- tests/user.test.js
+++ tests/user.test.js
@@ -27,6 +27,80 @@ beforeEach(async () => {
     }
 });
 
+test('POST /user', done => {
+    try {
+        // Correct inputs
+        request
+            .post('/user')
+            .send({email:'email1@example.com', password:'password'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, {email:'email1@example.com'})
+            .end(done);
+        // Conflict inputs
+        request
+            .post('/user')
+            .send({email:'email1@example.com', password:'password'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .end(done);
+        // Invalid email
+        request
+            .post('/user')
+            .send({email:'email1example.com', password:'password'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400)
+            .end(done);
+        // Invalid password; below minimum
+        request
+            .post('/user')
+            .send({email:'user1@example.com', password:'a'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400)
+            .end(done);
+        // Invalid password; above maximum
+        request
+            .post('/user')
+            .send({email:'user2@example.com', password:'sdjfasd jfklajscklasjdmcilas jmcojajsodfjasdolf jasofj asofj JSLFK NSIjfosdnmf CLSDIjfo'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400)
+            .end(done);
+        // Valid password with special characters, valid email
+        request
+            .post('/user')
+            .send({email:'user3@example.com', password:'!@#$%^&*() {}=-<>/?:;123'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, {email:'user3@example.com'})
+            .end(done);
+        // Valid password, valid email with dot
+        request
+            .post('/user')
+            .send({email:'user.4@example.com', password:'password'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, {email:'user.4@example.com'})
+            .end(done);
+        // 100 valid sign ups
+        for (var u = 0; u < 100; u++) {
+            var testEmail = `test.email.${u}@example.com`;
+            request
+                .post('/user')
+                .send({email:testEmail, password:'password'})
+                .set('Accept', 'application/json')
+                .expect('Content-Type', 'application/json; charset=utf-8')
+                .expect(200, {email:testEmail})
+                .end(done);
+        }
+    }
+    catch (error) {
+        console.error(error);
+    }
+}, (30 * 1000));
 
 afterAll(async () => {
     try {
