1d7f5dfaf316f11db62048788d54b44f75a345b8
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-12-06 21:37:46 -0500

    Implement POST /user endpoint

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,50 @@ const {body, validationResult} = require('express-validator');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+const getUserWithEmail = async email => {
+    try {
+        const targetUser = await UserModel.findOne({email}).exec();
+        return targetUser;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+const userValidators = [
+    body('email', 'Please enter a valid email.').isEmail(),
+    body('password', 'Password must be at least 5 characters and less than 32 characters.').isLength({min: 5, max:32})
+];
+router.post('/', userValidators, async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({error: errors.array()});
+        return next();
+    }
+
+    try {
+        const newEmail = req.body.email;
+        const newPassword = req.body.password;
+
+        const isUserExist = await getUserWithEmail(newEmail);
+        if (isUserExist) {
+            res.status(409).json({error:{msg:`A user with the email ${newEmail} already exists.`}})
+            return next();
+        }
+
+        const passwordHash = await bcrypt.hash(newPassword, saltRounds);
+        const newUser = {
+            email: newEmail,
+            password: passwordHash
+        };
+
+        await UserModel.create(newUser);
+        res.status(200).json({email: newEmail});
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({error:{msg:'Unable to create new user.'}});
+    }
+});
+
 module.exports = router;
