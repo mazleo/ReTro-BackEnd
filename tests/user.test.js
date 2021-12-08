ffad192634b4e09cda13e9850c7a6e1245f805b6
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-12-07 21:35:26 -0500

    Fix user endpoint tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -4,40 +4,56 @@ const supertest = require('supertest');
 const request = supertest(config.get('supertestUri'));
 const database = require('../model/database');
 const UserModel = require('../model/User');
+const EntityIdIndexerModel = require('../model/EntityIdIndexer');
 
 const resetUserDatabase = async () => {
-    return UserModel.deleteMany({}).exec();
+    await UserModel.deleteMany({}).exec();
 }
+
+const resetIndexer = async () => {
+    try {
+        await EntityIdIndexerModel.deleteMany({}).exec();
+
+        const indexer = {
+            userIdIndex: 0,
+            retroCalendarIdIndex: 0,
+            workspaceIdIndex: 0,
+            projectIdIndex: 0,
+            tagIdIndex: 0,
+            tagCompositionIdIndex: 0,
+            taskIdIndex: 0,
+            timesheetIdIndex: 0,
+            logIdIndex: 0
+        }
+
+        await EntityIdIndexerModel.create(indexer);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+const wait = waitMS => {
+    const beginTimeMS = Date.now();
+    let currentTimeMS = beginTimeMS;
+    while (currentTimeMS - beginTimeMS < waitMS) {
+        currentTimeMS = Date.now();
+    }
+};
 
 beforeAll(async () => {
     try {
         await database.connect();
-    }
-    catch (error) {
-        console.error(error);
-    }
-});
-
-beforeEach(async () => {
-    try {
         await resetUserDatabase();
+        await resetIndexer();
     }
     catch (error) {
         console.error(error);
     }
 });
 
-test('POST /user', done => {
+test('POST /user valid input', done => {
     try {
-        // Correct inputs
-        request
-            .post('/user')
-            .send({email:'email1@example.com', password:'password'})
-            .set('Accept', 'application/json')
-            .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200, {email:'email1@example.com'})
-            .end(done);
-        // Conflict inputs
         request
             .post('/user')
             .send({email:'email1@example.com', password:'password'})
@@ -45,7 +61,29 @@ test('POST /user', done => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200)
             .end(done);
-        // Invalid email
+    }
+    catch (error) {
+        console.error(error);
+    }
+}, (60 * 1000));
+
+test('POST /user with conflict input', done => {
+    try {
+        request
+            .post('/user')
+            .send({email:'email1@example.com', password:'password'})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(409, {error:{msg:"A user with the email email1@example.com already exists."}})
+            .end(done);
+    }
+    catch (error) {
+        console.error(error);
+    }
+});
+
+test('POST /user with invalid email', done => {
+    try {
         request
             .post('/user')
             .send({email:'email1example.com', password:'password'})
@@ -53,7 +91,14 @@ test('POST /user', done => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(400)
             .end(done);
-        // Invalid password; below minimum
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+test('POST /user with invalid password; below minimum length', done => {
+    try {
         request
             .post('/user')
             .send({email:'user1@example.com', password:'a'})
@@ -61,7 +106,14 @@ test('POST /user', done => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(400)
             .end(done);
-        // Invalid password; above maximum
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+test('POST /user with invalid password; above minimum length', done => {
+    try {
         request
             .post('/user')
             .send({email:'user2@example.com', password:'sdjfasd jfklajscklasjdmcilas jmcojajsodfjasdolf jasofj asofj JSLFK NSIjfosdnmf CLSDIjfo'})
@@ -69,72 +121,52 @@ test('POST /user', done => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(400)
             .end(done);
-        // Valid password with special characters, valid email
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+test('POST /user with valid input; password with special characters', done => {
+    try {
         request
             .post('/user')
             .send({email:'user3@example.com', password:'!@#$%^&*() {}=-<>/?:;123'})
             .set('Accept', 'application/json')
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200, {email:'user3@example.com'})
+            .expect(200)
             .end(done);
-        // Valid password, valid email with dot
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+test('POST /user with valid inputs; email with dot', done => {
+    try {
         request
             .post('/user')
             .send({email:'user.4@example.com', password:'password'})
             .set('Accept', 'application/json')
             .expect('Content-Type', 'application/json; charset=utf-8')
-            .expect(200, {email:'user.4@example.com'})
+            .expect(200)
             .end(done);
-        // 100 valid sign ups
-        for (var u = 0; u < 100; u++) {
-            var testEmail = `test.email.${u}@example.com`;
-            request
-                .post('/user')
-                .send({email:testEmail, password:'password'})
-                .set('Accept', 'application/json')
-                .expect('Content-Type', 'application/json; charset=utf-8')
-                .expect(200, {email:testEmail})
-                .end(done);
-        }
     }
     catch (error) {
-        console.error(error);
+        console.log(error);
     }
-}, (30 * 1000));
+});
 
 test('GET /user', done => {
-    // Create samples
+    // Create sample
     request
         .post('/user')
         .send({email:'get.user.test.1@email.com',password:'password'})
         .end(done);
-    request
-        .post('/user')
-        .send({email:'get.user.test.2@email.com',password:'password'})
-        .end(done);
-    request
-        .post('/user')
-        .send({email:'get.user.test.3@email.com',password:'password'})
-        .end(done);
-
-    // Test retrieval of samples
+    // Test retrieval of sample
     request
         .get('/user')
         .send({email:'get.user.test.1@email.com'})
-        .set('Accept', 'application/json')
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect(200)
-        .end(done);
-    request
-        .get('/user')
-        .send({email:'get.user.test.2@email.com'})
-        .set('Accept', 'application/json')
-        .expect('Content-Type', 'application/json; charset=utf-8')
-        .expect(200)
-        .end(done);
-    request
-        .get('/user')
-        .send({email:'get.user.test.3@email.com'})
         .set('Accept', 'application/json')
         .expect('Content-Type', 'application/json; charset=utf-8')
         .expect(200)
@@ -144,6 +176,7 @@ test('GET /user', done => {
 afterAll(async () => {
     try {
         await resetUserDatabase();
+        await resetIndexer();
         await database.close();
     }
     catch (error) {
