b59161098c2ba66614f894aafa5b3ca5989bbff8
Jonnelin Leonardo <jonnelin.leonardo@gmail.com>
2021-12-24 18:59:44 -0500

    Modify auth middleware variable names in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/auth');
+const { generateToken, validateToken } = require('../middleware/auth');
 
-router.post('/', authMiddleware.generateToken);
+router.post('/', generateToken);
 
 module.exports = router;
